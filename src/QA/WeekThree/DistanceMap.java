2ecbedb2ab253113585da742e4e6a870a10f7db8
nilyang <nil.yang@qq.com>
2016-07-28 21:36:34 +0800

    1.modify:代码整理

--- src/QA/WeekThree/DistanceMap.java
+++ src/QA/WeekThree/DistanceMap.java
@@ -12,44 +12,39 @@ public class DistanceMap {
 		// TODO Auto-generated constructor stub
 	}
 
-	public void add(String from, String dist, int distance)
-	{
+	public void add(String from, String dist, int distance) {
 		String key = makeKey(from, dist);
-		if(!distance_map.containsKey(key)){
+		if (!distance_map.containsKey(key)) {
 			distance_map.put(key, distance);
 		}
 	}
 
-	public int search(String from ,String dist)
-	{
-		return distance_map.get(makeKey(from,dist));
+	public int search(String from, String dist) {
+		return distance_map.get(makeKey(from, dist));
 	}
-	
-	public void print()
-	{
+
+	public void print() {
 		System.out.println(this.distance_map);
 	}
-	
- 	public static void main(String[] args) 
-	{
+
+	private String makeKey(String from, String dist) {
+		String key;
+		int comp = from.compareTo(dist);
+		if (comp > 0) {
+			key = dist + ":" + from;
+		} else {
+			key = from + ":" + dist;
+		}
+
+		return key;
+	}
+
+	public static void main(String[] args) {
 		DistanceMap dist = new DistanceMap();
 		dist.add("shanghai", "hangzhou", 123);
 		dist.add("wuhan", "hangzhou2", 23);
 		dist.add("gaosu", "hangzhou3", 3312);
 		dist.print();
-	}
-
-	private String makeKey(String from , String dist)
-	{
-		String key;
-		int comp = from.compareTo(dist); 
-		if(comp > 0){
-			key = dist + ":" + from;
-		}else{
-			key = from + ":" + dist;
-		}
-
-		return key;
 	}
 
 }
